962795d7f24bc1a98cdb7d910741aa46a2d60a31
B00TK1D <stearns.josiah@gmail.com>
2023-09-24 17:33:48 -0400

    Simplified, removed extra files

--- train.py
+++ train.py
@@ -2,16 +2,33 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from model import PimaClassifier
 
 
-# load the dataset, split into input (X) and output (y) variables
+# Load the dataset, split into input (X) and output (y) variables
 dataset = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
 X = dataset[:,0:8]
 y = dataset[:,8]
 
 X = torch.tensor(X, dtype=torch.float32)
 y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+
+
+# Define the model
+class PimaClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden1 = nn.Linear(8, 128)
+        self.act1 = nn.ReLU()
+        self.hidden2 = nn.Linear(128, 128)
+        self.act2 = nn.ReLU()
+        self.output = nn.Linear(128, 1)
+        self.act_output = nn.Sigmoid()
+ 
+    def forward(self, x):
+        x = self.act1(self.hidden1(x))
+        x = self.act2(self.hidden2(x))
+        x = self.act_output(self.output(x))
+        return x
 
 
 # Initialize the model
